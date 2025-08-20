9f61dfad6c9c7ed4d2b28781c4aefd69262a0865
opaulosaa <pauloarneto@gmail.com>
2025-08-19 21:19:58 -0300

    primeiro commit, alocando o projeto no repositório remoto

---
+++ src/main/java/com/ziplosa/BarbeariaZipLosa/BarbeariaZipLosaApplication.java
@@ -0,0 +1,13 @@
+package com.ziplosa.BarbeariaZipLosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaZipLosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaZipLosaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ziplosa/BarbeariaZipLosa/BarbeariaZipLosaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziplosa.BarbeariaZipLosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaZipLosaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
